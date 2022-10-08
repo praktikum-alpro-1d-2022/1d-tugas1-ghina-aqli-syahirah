68e080bebe7f0af6a3774b7a4f472199a449c5f0
Ghina Aqli Syahirah <114642992+ghinaaqli@users.noreply.github.com>
2022-10-08 22:23:35 +0700

    Add files via upload

---
+++ C++ (2) variabel sementara.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+const float PHI = 3.14;
+
+int main() {
+    float r = 2.5;
+    float L = PHI * r * r;
+
+    cout << "Luas lingkaran adalah: " << L << endl;
+
+    return 0;
+}
